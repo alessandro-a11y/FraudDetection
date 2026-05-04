1f55ce6dda900ed16e788b0ad30d8279da6ac157
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 22:16:02 -0300

    Create LocationMismatchRule.cs

---
+++ FraudDetection.Domain/Rules/LocationMismatchRule.cs
@@ -0,0 +1,14 @@
+﻿using FraudDetection.Domain.Entities;
+using FraudDetection.Domain.Interfaces;
+
+namespace FraudDetection.Domain.Rules;
+
+public class LocationMismatchRule : IFraudRule
+{
+    public string Name => "LocationMismatch";
+
+    public int Evaluate(Transaction transaction)
+    {
+        return transaction.Location != "BR" ? 30 : 0;
+    }
+}
