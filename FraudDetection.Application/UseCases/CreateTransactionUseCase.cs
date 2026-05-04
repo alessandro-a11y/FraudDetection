e23601feb6b4cec4c18d93aed5a1af7dd9e33852
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 22:16:13 -0300

    Update CreateTransactionUseCase.cs

--- FraudDetection.Application/UseCases/CreateTransactionUseCase.cs
+++ FraudDetection.Application/UseCases/CreateTransactionUseCase.cs
@@ -1,4 +1,5 @@
 using FraudDetection.Application.DTOs;
+using FraudDetection.Application.Services;
 using FraudDetection.Domain.Entities;
 using FraudDetection.Domain.Interfaces;
 
@@ -7,15 +8,22 @@ namespace FraudDetection.Application.UseCases;
 public class CreateTransactionUseCase
 {
     private readonly ITransactionRepository _repository;
+    private readonly FraudDetectionService _fraudService;
 
-    public CreateTransactionUseCase(ITransactionRepository repository)
+    public CreateTransactionUseCase(
+        ITransactionRepository repository,
+        FraudDetectionService fraudService)
     {
         _repository = repository;
+        _fraudService = fraudService;
     }
 
     public async Task<TransactionResponseDto> ExecuteAsync(CreateTransactionDto dto)
     {
         var transaction = new Transaction(dto.UserId, dto.Amount, dto.Location);
+
+        // Analisa antes de salvar
+        await _fraudService.AnalyzeAsync(transaction);
 
         await _repository.AddAsync(transaction);
 
