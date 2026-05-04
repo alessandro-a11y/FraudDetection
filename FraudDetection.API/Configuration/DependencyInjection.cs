8e663bca9f2d08bd21975d01976813c9d9e14c11
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 22:02:59 -0300

    Update DependencyInjection.cs

--- FraudDetection.API/Configuration/DependencyInjection.cs
+++ FraudDetection.API/Configuration/DependencyInjection.cs
@@ -1,16 +1,60 @@
-﻿using FraudDetection.Application.Services;
+﻿using System.Text;
+using FraudDetection.Application.Services;
 using FraudDetection.Application.UseCases;
 using FraudDetection.Domain.Interfaces;
+using FraudDetection.Infrastructure.Data;
+using FraudDetection.Infrastructure.Identity;
 using FraudDetection.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FraudDetection.API.Configuration;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddProjectDependencies(this IServiceCollection services)
+    public static IServiceCollection AddProjectDependencies(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        services.AddSingleton<ITransactionRepository, TransactionRepository>();
+        // Banco de dados
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
+        // Identity
+        services.AddIdentity<AppUser, IdentityRole>(options =>
+        {
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 6;
+        })
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
+        // JWT
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["Jwt:Issuer"],
+                ValidAudience = configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
+            };
+        });
+
+        services.AddScoped<TokenService>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
         services.AddScoped<CreateTransactionUseCase>();
         services.AddScoped<FraudDetectionService>();
 
