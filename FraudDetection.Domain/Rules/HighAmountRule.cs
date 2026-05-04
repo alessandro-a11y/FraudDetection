d029b984fafe623ae4926d1ee398473cf2fcdbac
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 22:16:03 -0300

    Create HighAmountRule.cs

---
+++ FraudDetection.Domain/Rules/HighAmountRule.cs
@@ -0,0 +1,17 @@
+﻿using FraudDetection.Domain.Entities;
+using FraudDetection.Domain.Interfaces;
+
+namespace FraudDetection.Domain.Rules;
+
+public class HighAmountRule : IFraudRule
+{
+    public string Name => "HighAmount";
+
+    public int Evaluate(Transaction transaction)
+    {
+        int score = 0;
+        if (transaction.Amount > 5000) score += 40;
+        if (transaction.Amount > 10000) score += 30;
+        return score;
+    }
+}
