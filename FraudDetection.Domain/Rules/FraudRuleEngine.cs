13aac44763511fd58a0603c821a251734c648742
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 22:16:05 -0300

    Create FraudRuleEngine.cs

---
+++ FraudDetection.Domain/Rules/FraudRuleEngine.cs
@@ -0,0 +1,27 @@
+﻿using FraudDetection.Domain.Entities;
+using FraudDetection.Domain.Interfaces;
+
+namespace FraudDetection.Domain.Rules;
+
+public class FraudRuleEngine
+{
+    private readonly IEnumerable<IFraudRule> _rules;
+
+    public FraudRuleEngine(IEnumerable<IFraudRule> rules)
+    {
+        _rules = rules;
+    }
+
+    public int Evaluate(Transaction transaction)
+    {
+        int totalScore = 0;
+
+        foreach (var rule in _rules)
+        {
+            var score = rule.Evaluate(transaction);
+            totalScore += score;
+        }
+
+        return totalScore;
+    }
+}
