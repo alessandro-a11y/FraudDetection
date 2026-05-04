a0afa1301c3fcc0babcf279343008f7142d53b83
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 22:02:01 -0300

    Create AppDbContextFactory.cs

---
+++ FraudDetection.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FraudDetection.Infrastructure.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        // Caminho absoluto direto para o appsettings.json da API
+        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../FraudDetection.API");
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        // Debug: vai aparecer no terminal
+        Console.WriteLine($"[Factory] BasePath: {basePath}");
+        Console.WriteLine($"[Factory] ConnectionString: {connectionString}");
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
