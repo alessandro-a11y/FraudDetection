44b831125fda130ed1df4a193d8c1e298b857c9a
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 22:15:59 -0300

    Create VelocityRule.cs

---
+++ FraudDetection.Domain/Rules/VelocityRule.cs
@@ -0,0 +1,21 @@
+﻿using FraudDetection.Domain.Entities;
+using FraudDetection.Domain.Interfaces;
+
+namespace FraudDetection.Domain.Rules;
+
+public class VelocityRule : IFraudRule
+{
+    private readonly List<Transaction> _recentTransactions;
+
+    public string Name => "Velocity";
+
+    public VelocityRule(List<Transaction> recentTransactions)
+    {
+        _recentTransactions = recentTransactions;
+    }
+
+    public int Evaluate(Transaction transaction)
+    {
+        return _recentTransactions.Count >= 3 ? 20 : 0;
+    }
+}
